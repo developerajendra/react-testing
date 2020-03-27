aed2472cde1d93b411e073df25ddbe343d243e57
Rajendra <rajendra@Rajendras-MacBook-Pro.local>
2020-03-27 15:02:55 +0530

    Increment state testing implementation

---
+++ build/precache-manifest.16b80c59847a09866c32ef938605a0fd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "183763423b822e52ff9750d57650f39c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aa39bd24cf7fadafcf01",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "cc86f92a0e9b1ed7b6f7",
+    "url": "/static/js/2.1b35cb52.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.1b35cb52.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa39bd24cf7fadafcf01",
+    "url": "/static/js/main.c21e9602.chunk.js"
+  },
+  {
+    "revision": "415e9bcf6c081ea50565",
+    "url": "/static/js/runtime-main.f8c5b4be.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
